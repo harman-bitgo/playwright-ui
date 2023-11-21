435258088e05cab550a71d2bbd6a9fca21399b49
Harman Singh <harmansingh@bitgo.com>
2023-11-21 17:25:27 +0530

    feat: Playwright-UI base skeleton

---
+++ Framework/signIn.ts
@@ -0,0 +1,23 @@
+import { expect } from "@playwright/test";
+
+const base_url= "https://app.bitgo-test.com/";
+
+
+export async function manualSignIn({ page }){
+
+    await page.goto(base_url);
+    await expect(page).toHaveTitle('Login - BitGo');
+    await page.getByLabel('Email Address').fill("qaindiauser@gmail.com");
+    await page.getByLabel('Password').fill("#Bondiola1234");
+    await page.getByText('Log in').click();
+    await page.getByLabel('2FA Code').fill('000000');
+    await page.getByText('Verify').click();
+    await page.waitForTimeout(10000);
+    await expect(page.locator("xpath=//button[@aria-label='Account']")).toBeVisible();
+}
+
+export async function loginValidationusingCookies({ page }) {
+
+    await page.goto(base_url);
+    await expect(page.locator("xpath=//button[@aria-label='Account']")).toBeVisible();
+}
---
+++ tests/cookieTests.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+import * as SignIn from '../Framework/signIn';
+
+
+test.beforeAll('Manual login validation', async ({ browser })=>{
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await SignIn.manualSignIn( {page} );
+    const cntx = page.context();
+    await cntx.storageState({path:"./auth.json"});
+});
+
+test('Login Cookie validation', async ({ browser })=>{
+
+    const context = await browser.newContext({
+        storageState: './auth.json'
+    })
+    const page = await context.newPage();
+    await SignIn.loginValidationusingCookies( {page} );
+});
